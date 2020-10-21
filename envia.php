2d332e7af6dbee6d2282327cd810a5140864a9cc
Laura Nayelly Galan Camargo <lauracamargo.1d@gmail.com>
2020-10-21 02:38:56 -0500

    listo repositorio

---
+++ envia.php
@@ -0,0 +1,21 @@
+﻿<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$para = 'lauracamargo.1d@gmail.com'; 
+$asunto = 'Consulta'; 
+
+$mensaje = "
+    Nombre del remitente: ".$nombre."
+    Correo: ".$email."
+    Telefono: ".$telefono.."
+    Mensaje: ".$mensaje. " 
+    ";
+
+mail($para,$asunto,utf8_decode($mensaje));
+
+header("location: index.html");
+
+?>
